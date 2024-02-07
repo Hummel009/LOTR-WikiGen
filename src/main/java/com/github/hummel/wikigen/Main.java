8e0bb4469616e652855e7ed1dee005891e5f8aad
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-07 18:21:25 +0300

    Update Main.java

--- src/main/java/com/github/hummel/wikigen/Main.java
+++ src/main/java/com/github/hummel/wikigen/Main.java
@@ -5,7 +5,6 @@ import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
 @Mod(modid = "wikigen", dependencies = "required-after:lotr", useMetadata = true)
 public class Main {
-
 	@Mod.EventHandler
 	public void serverStarting(FMLServerStartingEvent e) {
 		e.registerServerCommand(new DbCommand());
